df465d1784240ba2916dcbaeb4fa49048286950a
Sproker <janerik5@tlu.ee>
2023-01-11 20:59:30 +0200

    Finished responsive product listing page & added semi-responsive footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductList from './pages/ProductList';
-import Navbar from './components/Navbar'
+import AddProduct from './pages/AddProduct';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <BrowserRouter>
+            <div className="content">
             <Navbar />
                 <Routes>
-                    <Route exact path='/' element={<ProductList />} />
+                    <Route exact path='/' element = {<ProductList />} />
+                    <Route path='/add' element = {<AddProduct />} />
                 </Routes>
+            <Footer />
+            </div>
         </BrowserRouter>
     );
 }
